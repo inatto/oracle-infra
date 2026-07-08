a4ceee771ae0c4307f21012c75734fc52ac89d3b
Daniel <danielmaiax@gmail.com>
2026-07-08 14:03:11 -0300

    ...

---
+++ database/selects/select-content_attach.sql
@@ -0,0 +1,13 @@
+-- noinspection SqlConstantExpressionForFile
+
+
+SELECT *
+FROM content_attachment
+WHERE 1 = 1
+--     AND ctype = 'news'
+--     AND ctype = 'diretoria'
+--     AND ctype = 'diretoria'
+--     AND ctype = 'estatuto'
+ORDER BY id DESC
+;
+
