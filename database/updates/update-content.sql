58a3b3ea93662899e0b32f2f66bc260380c1e17d
Daniel <danielmaiax@gmail.com>
2026-07-08 19:44:49 -0300

    ...

---
+++ database/updates/update-content.sql
@@ -0,0 +1,143 @@
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-outubro-a-dezembro-19.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-outubro-a-dezembro-19.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 257 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-julho-a-setembro-19.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-julho-a-setembro-19.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 256 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-abril-a-junho-19.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-abril-a-junho-19.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 255 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-janeiro-a-julho-20.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-janeiro-a-julho-20.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 254 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-julho-a-dezembro-20.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-julho-a-dezembro-20.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 253 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-janeiro-a-marco-2021.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-janeiro-a-marco-2021.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 252 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-abril-a-agosto-2021.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-abril-a-agosto-2021.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 251 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-setembro-a-dezembro-2021.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-setembro-a-dezembro-2021.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 250 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-janeiro-a-marco-2022.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-janeiro-a-marco-2022.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 249 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-abril-a-junho-2022.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-abril-a-junho-2022.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 248 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-julho-a-dezembro-2022.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-julho-a-dezembro-2022.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 247 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-janeiro-a-julho-2023.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-janeiro-a-julho-2023.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 246 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-agosto-a-dezembro-2023.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-agosto-a-dezembro-2023.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 245 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-junho-2025.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-junho-2025.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 244 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+UPDATE content SET
+  image_mime_type = 'image/webp',
+  image_filename = 'jornal-anpprev-janeiro-2026.webp',
+  image_url = '/tenants/anpprev/images/journals/jornal-anpprev-janeiro-2026.webp',
+  image_alt = title,
+  image_updated_at = SYSTIMESTAMP
+WHERE id = 243 AND tenant_code = 'anpprev' AND ctype_code = 'journal';
+
+COMMIT;
+
+
+
+
+/*
+20260708-1941
+UPDATE content SET title = 'Jornal ANPPREV - Outubro a Dezembro/2019' WHERE id = 257;
+UPDATE content SET title = 'Jornal ANPPREV - Julho a Setembro/2019' WHERE id = 256;
+UPDATE content SET title = 'Jornal ANPPREV - Abril a Junho/2019' WHERE id = 255;
+UPDATE content SET title = 'Jornal ANPPREV - Janeiro a Julho/2020' WHERE id = 254;
+UPDATE content SET title = 'Jornal ANPPREV - Julho a Dezembro/2020' WHERE id = 253;
+UPDATE content SET title = 'Jornal ANPPREV - Janeiro a Março/2021' WHERE id = 252;
+UPDATE content SET title = 'Jornal ANPPREV - Abril a Agosto/2021' WHERE id = 251;
+UPDATE content SET title = 'Jornal ANPPREV - Setembro a Dezembro/2021' WHERE id = 250;
+UPDATE content SET title = 'Jornal ANPPREV - Janeiro a Março/2022' WHERE id = 249;
+UPDATE content SET title = 'Jornal ANPPREV - Abril a Junho/2022' WHERE id = 248;
+UPDATE content SET title = 'Jornal ANPPREV - Julho a Dezembro/2022' WHERE id = 247;
+UPDATE content SET title = 'Jornal ANPPREV - Janeiro a Julho/2023' WHERE id = 246;
+UPDATE content SET title = 'Jornal ANPPREV - Agosto a Dezembro/2023' WHERE id = 245;
+UPDATE content SET title = 'Jornal ANPPREV - Junho/2025' WHERE id = 244;
+UPDATE content SET title = 'Jornal ANPPREV - Janeiro/2026' WHERE id = 243;
+*/
