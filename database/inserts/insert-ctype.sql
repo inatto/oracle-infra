c397e7294826a1fd1b79cb40c34f29d25f1c52dc
Daniel <danielmaiax@gmail.com>
2026-07-08 21:56:40 -0300

    ...

---
+++ database/inserts/insert-ctype.sql
@@ -0,0 +1,106 @@
+
+INSERT INTO CTYPE (
+    ID,
+    TENANT_CODE,
+    CODE,
+    ID_PERFIL_OLD,
+    TAG_PERFIL_OLD,
+    NAME,
+    PLURAL_NAME,
+    DESCRIPTION,
+    READ_ONLY,
+    IS_ADMIN,
+    ACTIVE
+) VALUES (
+    44,
+    'anpprev',
+    'bulletin',
+    NULL,
+    NULL,
+    'Informe',
+    'Informes',
+    'Informes institucionais',
+    0,
+    0,
+    1
+);
+
+INSERT INTO CTYPE (
+    ID,
+    TENANT_CODE,
+    CODE,
+    ID_PERFIL_OLD,
+    TAG_PERFIL_OLD,
+    NAME,
+    PLURAL_NAME,
+    DESCRIPTION,
+    READ_ONLY,
+    IS_ADMIN,
+    ACTIVE
+) VALUES (
+    45,
+    'anpprev',
+    'note',
+    NULL,
+    NULL,
+    'Nota',
+    'Notas',
+    'Notas institucionais',
+    0,
+    0,
+    1
+);
+
+INSERT INTO CTYPE (
+    ID,
+    TENANT_CODE,
+    CODE,
+    ID_PERFIL_OLD,
+    TAG_PERFIL_OLD,
+    NAME,
+    PLURAL_NAME,
+    DESCRIPTION,
+    READ_ONLY,
+    IS_ADMIN,
+    ACTIVE
+) VALUES (
+    46,
+    'anpprev',
+    'regulation',
+    NULL,
+    NULL,
+    'Norma',
+    'Normas',
+    'Normas e documentos normativos',
+    0,
+    0,
+    1
+);
+
+INSERT INTO CTYPE (
+    ID,
+    TENANT_CODE,
+    CODE,
+    ID_PERFIL_OLD,
+    TAG_PERFIL_OLD,
+    NAME,
+    PLURAL_NAME,
+    DESCRIPTION,
+    READ_ONLY,
+    IS_ADMIN,
+    ACTIVE
+) VALUES (
+    47,
+    'anpprev',
+    'useful_link',
+    NULL,
+    NULL,
+    'Link Útil',
+    'Links Úteis',
+    'Links úteis institucionais',
+    0,
+    0,
+    1
+);
+
+COMMIT;
