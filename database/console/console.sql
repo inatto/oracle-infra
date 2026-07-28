7f16d80696513f83097783bf76584d2cb1f27375
Daniel <danielmaiax@gmail.com>
2026-07-28 01:38:28 -0300

    ...

--- database/console/console.sql
+++ database/console/console.sql
@@ -1,36 +1,12 @@
-MERGE INTO etype dst
-USING (
-    SELECT
-        'sinproprev' AS tenant_code
-        , 'associate' AS code
-        , 'Associado' AS name
-        , 'Associados' AS plural_name
-        , 1 AS active
-    FROM dual
-) src
-ON (
-    dst.tenant_code = src.tenant_code
-        AND dst.code = src.code
-    )
-WHEN MATCHED THEN
-    UPDATE SET
-        dst.name = src.name
-               , dst.plural_name = src.plural_name
-               , dst.active = src.active
-WHEN NOT MATCHED THEN
-    INSERT (
-        tenant_code
-    , code
-    , name
-    , plural_name
-    , active
-    )
-    VALUES (
-        src.tenant_code
-    , src.code
-    , src.name
-    , src.plural_name
-    , src.active
-    );
-
-COMMIT;
+BEGIN
+    EXECUTE IMMEDIATE '
+    ALTER TABLE email_campaign
+        DROP CONSTRAINT uk_email_campaign_stats
+    ';
+EXCEPTION
+    WHEN OTHERS THEN
+        IF SQLCODE != -2443 THEN
+            RAISE;
+        END IF;
+END;
+/
