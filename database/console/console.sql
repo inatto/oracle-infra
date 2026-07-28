7f782dd3f8ccc6b804063bb3bad0585fca46849f
Daniel <danielmaiax@gmail.com>
2026-07-28 15:11:07 -0300

    ...

--- database/console/console.sql
+++ database/console/console.sql
@@ -1,33 +1,7 @@
-SELECT
-    p.tenant_code
-    , p.parent_code
-    , p.role
-    , t1.name AS tenant_name
-    , t2.name AS parent_name
-FROM partnership p
-JOIN tenant t1
-     ON t1.code = p.tenant_code
-JOIN tenant t2
-     ON t2.code = p.parent_code
-WHERE p.tenant_code IN ('sinproprev', 'anpprev')
-    OR p.parent_code IN ('sinproprev', 'anpprev')
-ORDER BY
-    p.tenant_code
-    , p.parent_code;
-
-INSERT INTO partnership (
-    tenant_code
-, parent_code
-, role
-)
-SELECT
-    'anpprev'
-    , 'sinproprev'
-    , 'partner'
-FROM dual
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM partnership
-    WHERE tenant_code = 'anpprev'
-        AND parent_code = 'sinproprev'
-);
+SELECT column_name
+FROM user_tab_cols
+WHERE table_name = 'MEMBER_APPLICATION'
+    AND NVL(identity_column, 'NO') = 'NO'
+    AND NVL(virtual_column, 'NO') = 'NO'
+    AND NVL(hidden_column, 'NO') = 'NO'
+ORDER BY column_id
