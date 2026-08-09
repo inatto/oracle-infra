d67f41675ec8b007e7d0eecfd69c9d39a2ef7dce
Daniel <danielmaiax@gmail.com>
2026-08-09 04:41:50 -0300

    ...

--- database/console/console-oracle-infra.sql
+++ database/console/console-oracle-infra.sql
@@ -1,2 +1,6 @@
-ALTER TABLE BR_SITUACAO_ASSOCIATIVA
-    MODIFY (CODE VARCHAR2(30 CHAR));
+UPDATE entity
+SET md5_password = 'a31c3bb7fbe0d25ee3d36796b4dd23fd'
+WHERE id = 10351
+    AND LOWER(TRIM(email)) = 'dev@inatto.com';
+
+COMMIT;
