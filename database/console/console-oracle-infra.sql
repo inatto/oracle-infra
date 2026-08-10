d045c105281b371b0f7ac1c8c2cd8815bef4fd52
Daniel <danielmaiax@gmail.com>
2026-08-10 15:18:41 -0300

    ...

--- database/console/console-oracle-infra.sql
+++ database/console/console-oracle-infra.sql
@@ -1,1 +1,19 @@
-DROP INDEX UK_MEMBER_TENANT_SIAPE;
+INSERT INTO ETYPE (
+    TENANT_CODE,
+    CODE,
+    NAME,
+    PLURAL_NAME,
+    DESCRIPTION,
+    READ_ONLY,
+    IS_ADMIN,
+    ACTIVE
+) VALUES (
+    'asaclub',
+    'unknown',
+    'Desconhecido',
+    'Desconhecidos',
+    'Perfil legado não informado',
+    1,
+    0,
+    1
+);
